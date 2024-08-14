8dfbf0f2d6efb794c7c7a048d08715da0f29a6c9
David Besonen <david.besonen@huskers.unl.edu>
2024-08-14 09:03:43 -0500

    Initial commit

---
+++ lib/boc_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "boc_gem/version"
+
+module BocGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/boc_gem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BocGem
+  VERSION = "0.1.0"
+end
